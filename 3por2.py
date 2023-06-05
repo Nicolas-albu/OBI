7b1a64ccc1043259cc2fd3281d47af07ed0be10d
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-06-05 14:04:50 -0300

    adding 3 by 2 exercise

---
+++ 3por2.py
@@ -0,0 +1,25 @@
+N = int(input())
+
+precos = sorted([int(input()) for _ in range(N)], reverse=True)
+
+# grupos = []
+# resultado: int = 0
+
+# Minha 1 ° solução
+# for index in range(N):
+#     if index % 3 == 0:
+#         grupos.append(precos[index : index + 3])
+
+#         if len(grupos[-1]) > 2:
+#             grupos[-1].pop()
+#         resultado += sum(grupos[-1])
+
+# Solução do ChatGPT
+# for index in range(N):
+#     if index % 3 != 2:
+#         resultado += precos[index]
+
+# Minha 2° solução
+resultado = sum(precos[index] for index in range(N) if index % 3 != 2)
+
+print(resultado)
