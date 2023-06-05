65ce4fb9ef2d694722405186633bbfc4da1019cc
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-06-05 14:05:50 -0300

    add calculation exercise

---
+++ calculo.py
@@ -0,0 +1,9 @@
+S, A, B = [int(input()) for _ in range(3)]
+
+sum_digits = lambda number: sum(int(digit) for digit in number)
+
+numbers: list[int] = [
+    number for number in range(A, B + 1) if sum_digits(str(number)) == S
+]
+
+print(len(numbers))
